5198b63694ba5fa49f7a32b78d5d8603dd9d2881
Leandro5454 <leandrogimenez991@gmail.com>
2024-11-05 15:27:36 -0300

    ordenes de compra

--- index.js
+++ index.js
@@ -946,32 +946,15 @@ app.get('/api/ordenes_de_compra/:id/recepcion_productos', async (req, res) => {
 
 // Endpoint para agregar una nueva orden de compra
 app.post('/api/ordenes_de_compra/crear-orden', async (req, res) => {
-    const { id_proveedor, productos } = req.body; // Datos enviados desde el frontend
+    const { id_proveedor, total, productos } = req.body; // Datos enviados desde el frontend
 
-    if (!id_proveedor || !productos || productos.length === 0) {
-        return res.status(400).json({ error: 'Debe proporcionar un proveedor y al menos un producto' });
+    if (!id_proveedor || !total || !productos || productos.length === 0) {
+        return res.status(400).json({ error: 'Debe proporcionar un proveedor, un total y al menos un producto' });
     }
 
     try {
         const db = await getConnection();
         await db.beginTransaction(); // Iniciar una transacción
-
-        // Calcular el total sumando los precios de los productos multiplicados por la cantidad
-        let total = 0;
-        for (const producto of productos) {
-            const [productoData] = await db.query('SELECT precio FROM productos WHERE id_producto = ?', [producto.id_producto]);
-            
-            // Verificar si se encontró el producto
-            if (productoData.length > 0) {
-                const precio = productoData[0].precio;
-                total += precio * producto.cantidad;
-            } else {
-                // Manejar el caso donde no se encuentra el producto
-                console.error(`Producto con ID ${producto.id_producto} no encontrado`);
-                // Puedes lanzar un error o continuar con el siguiente producto
-                // throw new Error(`Producto con ID ${producto.id_producto} no encontrado`); 
-            }
-        }
 
         // Insertar la nueva orden de compra en la tabla `ordenes_de_compra`
         const [result] = await db.query(
@@ -996,7 +979,6 @@ app.post('/api/ordenes_de_compra/crear-orden', async (req, res) => {
         res.status(500).json({ error: 'Error al agregar la orden de compra', details: err.message });
     }
 });
-
 //--------------------------ENPOINTS PARA TOKEN---------------------------------
 
 // Endpoint para crear solicitud de mecánico
