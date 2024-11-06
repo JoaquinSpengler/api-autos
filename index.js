6a61bdd7d145244968e6d46359ccb0690a0de8a3
Joaquin Spengler <juacospengler@gmail.com>
2024-11-06 19:33:14 -0300

    cambios

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
     origin: ['localhost:5173','http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5175', 'http://localhost:5180',"https://radiador-spring-tp.vercel.app"], // Permitir ambos orígenes
     methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
 }));
 
