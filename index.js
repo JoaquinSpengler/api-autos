6b54ddc457ed63ba5bf9755303cf6c7052f0f04d
Federico Zapata <federico11zapata@gmail.com>
2024-11-11 07:04:04 -0300

    fix cors

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ const app = express();
 // Configuración de CORS
 app.use(cors({
     origin: [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'http://localhost:5175',
-        'http://localhost:5180',
-        'http://localhost:5182',
-        'https://radiador-spring-tp.vercel.app'
+        '*'
       ], // Permitir ambos orígenes
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
